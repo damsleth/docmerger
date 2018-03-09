ae5d2e3305dfd93aa5b4b8dff19c0b212bc1753f
Kim Damsleth <kim@damsleth.com>
2018-03-09 11:42:00 +0100

    initial commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+# DSS-PDFPROXY
+
+A simple node.js proxy server, acting as middleware between the client and OWA/WOPI/Word Automation Services.
+
+## Usage
+Pass a url to a pdf to the server, and it will return the PDF to the user, bypassing CORS
+
+## Prerequisites / Dependencies
+* nodejs v.8.9.4 (LTS) installed on machine running the server
+    * express
+    * node-fetch
+    * nodemon (for dev-purposes)
+
+## Installation
+* npm install
+* node index.js
+
+
+## maintainers
+DSS-Team, mainly @damsleth
---
+++ index.js
@@ -0,0 +1,23 @@
+var fetch = require("node-fetch")
+var fs = require("fs")
+var express = require('express')
+var request = require('request')
+
+var app = express();
+app.use('/pdfproxy', function (req, res) {
+    var url = req.url.replace('/?url=', '');
+    req.pipe(request(url)).pipe(res);
+});
+app.listen(process.env.PORT || 3000);
+
+function fetchPdf(url) {
+    fetch(url).then(res => {
+        res.arrayBuffer().then(data => {
+
+            // TODO: Return file to user 
+
+            // POC Saves file to disk.
+            fs.appendFileSync("test.pdf", new Buffer(data))
+        })
+    })
+}
