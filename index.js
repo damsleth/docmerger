76d86aca6238e557c61257086a22810eb87c5887
Kim Damsleth <kim@damsleth.com>
2018-03-15 12:47:01 +0100

    deleting temp files on completion

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var fetch = require("node-fetch")
 var fs = require("fs")
 var express = require("express")
-var request = require("request")
 var PDFMerge = require("pdf-merge")
 var app = express();
 app.listen(process.env.PORT || 7000);
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 // TODO: limit proxy requests to current top-level-domain only, to avoid proxy exploitation
-// TODO: UI Stuff
+// TODO: Delete temp files
 
 async function writeFile(path, data, opts = 'utf8') {
     new Promise((res, rej) => {
@@ -24,7 +23,7 @@ const SAVE_FOLDER = process.env.TEMP ? process.env.TEMP : process.env.TMPDIR
 app.use('/getpdf', (req, res) => {
     let d = (Object.keys(req.body).length > 0) ? req.body : req.query;
     if (!Object.keys(d).length) { returnHTMLBlob(res, `Error: no POST data was sent`) } else {
-        mergePDFs(d.documents).then((buffer) => {
+        MergePDFs(d.documents).then((buffer) => {
             res.writeHead(200, {
                 'Content-Type': 'application/pdf',
                 'Content-Disposition': 'filename=merged.pdf',
@@ -35,7 +34,23 @@ app.use('/getpdf', (req, res) => {
     }
 });
 
-async function mergePDFs(documents) {
+function RemoveTempFiles(tempFiles) {
+    tempFiles.map(file => {
+        fs.unlink(file, function (err) {
+            if (err && err.code == 'ENOENT') {
+                // file doens't exist
+                console.info(`File ${file} doesn't exist, won't remove it.`);
+            } else if (err) {
+                // other errors, e.g. maybe we don't have enough permission
+                console.error(`Error occurred while trying to remove file ${file}`);
+            } else {
+                console.info(`Temp file ${file} deleted`);
+            }
+        });
+    })
+}
+
+async function MergePDFs(documents) {
     console.log("merging documents, please stand by")
     let pdfPathArr = [];
     for (let i in documents) {
@@ -46,7 +61,9 @@ async function mergePDFs(documents) {
         console.log("saved " + doc.title + " to disk")
         pdfPathArr.push(`${SAVE_FOLDER}${doc.title}.pdf`)
     }
-    return await PDFMerge(pdfPathArr)
+    let pdfBuffer = await PDFMerge(pdfPathArr)
+    RemoveTempFiles(pdfPathArr)
+    return pdfBuffer
 }
 
 // Fetches pdfs from the local (server) filesystem.
@@ -79,6 +96,5 @@ app.use('/', (req, res) => {
     returnHTMLBlob(res, `
 <h2>This is a pdf proxy API</h2>
 <ul><li>/getpdf?[url] - returns a merged pdf from a set of OWA document IDs</li>
-<li>/getlocalpdf?[url] - merge and return pdfs from the server filesystem - <b>dev only</b></li>
-`)
+<li>/getlocalpdf?[url] - merge and return pdfs from the server filesystem - <b>dev only</b></li>`)
 });
