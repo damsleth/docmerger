83392c13d230ddcc184a4352b76ba582cc23ecd6
Kim Damsleth <kim@damsleth.com>
2020-05-08 09:26:09 +0200

    Now supports "merging" of 1 document

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
  * This app takes a list of pdfs, merges them and returns them as a single PDF to the client
  * This proxy is running on port 7001, and accepts POSTS to /getpdf with an object like this 
  * { documents:[{Title:string, Url:string:SpmNr:string}]}
+ * 
+ * Last updated 8.5.2020 by @damsleth
  */
 
 var fetch = require("node-fetch")
@@ -14,8 +16,8 @@ app.listen(process.env.PORT || 7002);
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 
-// If We're debugging, enable cors.
-// Debugging enabled on MacOS, aka darwinq
+// If We're debugging, enable CORS, so we accept calls from anywhere.
+// Debugging enabled on MacOS, aka darwin
 if (process.platform === "darwin") {
     var cors = require('cors');
     console.log(`Debugging, CORS enabled`)
@@ -28,21 +30,21 @@ if (process.platform === "darwin") {
     }));
 }
 
+// Log lastModifiedDate to console on startup.
 fs.stat(__filename.split(/[\\/]/).pop(), (err, stat) => {
     console.log(`PDF merger up and running\nLast modified ${stat.mtime.toDateString()}`)
 })
 
-//temp dir environment variables differ across platforms
+// Temp dir environment variables differ across platforms.
 const SAVE_FOLDER = process.env.TEMP ? process.env.TEMP : process.env.TMPDIR
 
-// Main endpoint that returns a merged PDF
+// Main endpoint that returns a merged PDF.
 app.use('/getpdf', (req, res) => {
     try {
         let d = (Object.keys(req.body).length > 0) ? req.body : req.query;
-        //when no data is sent
+
+        //When no data is sent, return error.
         if (!Object.keys(d).length) { returnHTMLBlob(res, `Error: no POST data was sent`) } else {
-            // when only one document is passed
-            if (d.documents.length === 1) { return fetchSinglePdf(res, d.documents[0]) }
 
             MergePDFs(d.documents).then((buffer) => {
                 res.writeHead(200, {
@@ -62,27 +64,8 @@ app.use('/getpdf', (req, res) => {
     }
 });
 
-function fetchSinglePdf(res, document) {
-    try {
-        fetch(document.Url).then(doc => {
-            doc.arrayBuffer().then(data => {
-                let pdfData = Buffer.from(data)
-                res.writeHead(200, {
-                    'Content-Type': 'application/pdf',
-                    'Content-Disposition': 'attachment; filename=' + document.Title + '.pdf',
-                    'Content-Length': pdfData.length
-                });
-                res.end(pdfData);
-            })
-        })
-    } catch (err) {
-        res.writeHead(500, {
-            "Content-Type": "text/html"
-        })
-        res.send(`Error in fetchSinglePdf. stacktrace: ${err}`)
-    }
-}
-
+// Main MergePDF function.
+// Ttakes an object with an array of { documents:[{Title:string, Url:string:SpmNr:string}] } as input.
 async function MergePDFs(documents) {
     console.log(`Saving ${documents.length} documents to disk`)
     let pdfs = [];
@@ -91,7 +74,8 @@ async function MergePDFs(documents) {
         let doc = documents[i]
         let pdf = await fetch(doc.Url)
         let data = await pdf.arrayBuffer()
-        // 6 character (sometimes 5) random hash value
+        // Files are saved to disk as 6 character (sometimes 5) random ASCII strings with .pdf extension
+        // This is to avoid issues with special character filenames
         let title = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6);
         await writeFile(`${SAVE_FOLDER}/${title}.pdf`, Buffer.from(data))
         console.log(`${nr}/${documents.length}: ${title} saved to disk. (Original title: "${doc.Title}")`)
@@ -103,6 +87,10 @@ async function MergePDFs(documents) {
     // let sortedPdfs = pdfs.sort((a, b) => a.spmnr - b.spmnr).map(p => p.path)
     let sortedPdfs = pdfs.map(p => p.path)
     console.log(`Merging documents...`)
+
+    // Corner case where there's only one document to "merge".
+    // We have to add a blank entry to avoid pdftk bombing with "You need at least two pdfs to merge"-warning.
+    if (sortedPdfs.length === 1) { sortedPdfs.push(" ") }
 
     return PDFMerge(sortedPdfs).then(buffer => {
         if (buffer) { console.log("Documents successfully merged, removing temp files") }
@@ -125,10 +113,10 @@ function RemoveTempFiles(tempFiles) {
     tempFiles.map(file => {
         fs.unlink(file.path, function (err) {
             if (err && err.code == 'ENOENT') {
-                // file doesn't exist
+                // File doesn't exist.
                 console.info(`File ${file.path} doesn't exist, won't remove it.`);
             } else if (err) {
-                // other errors, e.g. maybe we don't have enough permission
+                // Other errors, e.g. maybe running user doesn't have permission to delete files on disk.
                 console.error(`Error occurred while trying to remove file ${file.path}`);
             } else {
                 console.info(`Temp file ${file.path} deleted`);
@@ -146,8 +134,8 @@ function returnHTMLBlob(res, htmlBlob) {
     `)
 }
 
-// default root, display msg
-app.use('/', (req, res) => {
+// Default root endpoint, display a message
+app.use('/', (_req, res) => {
     returnHTMLBlob(res, `
 <h2>This is a pdf proxy API</h2>
 <ul><li><b>usage:</b> /getpdf?[url] - returns a merged pdf from an array of document urls</li>`)
