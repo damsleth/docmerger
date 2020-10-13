3b1f91ea0c923f07456639f1540d8db08edd05a2
Kim Damsleth <kim@damsleth.com>
2020-10-13 09:55:22 +0200

    returning single pdf buffer instead of saving to disk first

--- mergePDF.js
+++ mergePDF.js
@@ -1,53 +1,33 @@
 const PDFMerge = require('pdf-merge')
-const fs = require('fs')
 const fetch = require('node-fetch')
 const Utils = require('./utils')
 const SAVE_FOLDER = process.env.TEMP ? process.env.TEMP : process.env.TMPDIR
 
 module.exports = async function (documents) {
-
-    console.log(`Saving ${documents.length} documents to disk`)
+    documents.length === 1
+        ? console.log(`Single pdf requested (ekspedering?)\nReturning the buffer without saving to disk`)
+        : console.log(`Saving ${documents.length} documents to disk`)
     let pdfs = []
     for (let i in documents) {
         let nr = parseInt(i, 10) + 1
         let doc = documents[i]
         let pdf = await fetch.default(doc.Url)
         let pdfBuffer = await pdf.arrayBuffer()
-        let title = Utils.getRandomFilename() // generate random filename to avoid collisions
+        if (documents.length === 1) { // Corner case where there's only one document to "merge".
+            return Buffer.from(pdfBuffer)
+        }
+        let title = Utils.getRandomFilename() // generate random filename to avoid collisions'
         await Utils.writeFile(`${SAVE_FOLDER}/${title}.pdf`, Buffer.from(pdfBuffer))
         console.log(`${nr}/${documents.length}: ${title} saved to disk. (Original title: "${doc.Title}")`)
         pdfs.push({ path: `${SAVE_FOLDER}/${title}.pdf`, spmnr: doc.SpmNr })
     }
     console.log(`${documents.length} documents successfully saved`)
 
-    // We could do sorting here too, but it's handled client side for simplicity
-    // let sortedPdfs = pdfs.sort((a, b) => a.spmnr - b.spmnr).map(p => p.path)
-    let sortedPdfs = pdfs.map(p => p.path)
+    let sortedPdfs = pdfs.map(p => p.path) // Sorting is handled client side for simplicity
     console.log(`Merging documents...`)
-
-    let buffer;
-    // Corner case where there's only one document to "merge".
-    // just get the buffer from the file on disk
-    // fs.promises is not entirely stable in node 10, so we're playing it safe using trad fs.readFile
-    if (sortedPdfs.length === 1) {
-        try {
-            fs.readFile(sortedPdfs[0], function (err, data) {
-                if (err) throw err
-                buffer = data
-                console.log(`Single document "${sortedPdfs[0]}" loaded into buffer`)
-                // Clean up
-                Utils.removeTempFiles(pdfs);
-                return
-            })
-        }
-        catch (err) { console.error(err) }
-    } else {
-        buffer = await PDFMerge(sortedPdfs);
-        if (buffer) { console.log("Documents successfully merged, removing temp files"); }
-        // Clean up
-        Utils.removeTempFiles(pdfs);
-    }
-    console.log(`Temp files deleted, returning merged pdf to user`);
+    const buffer = await PDFMerge(sortedPdfs);
+    console.log("Documents successfully merged, removing temp files");
+    Utils.removeTempFiles(pdfs); // Clean up
     return buffer;
 
 }
